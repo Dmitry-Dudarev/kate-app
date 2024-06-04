7607c4a29be3eaada0dbc49ffe74910518af968e
Dmitry Dudarev <dr.nt2017@yandex.ru>
2024-06-04 15:42:12 +0300

    feat: планшет горизонтально

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -2,60 +2,69 @@ import React from "react";
 import "./ContactInfo.css";
 import { AppText } from "../constants/App-text";
 
-function ContactInfo(props){
-  const custonMargin = (props.position === "contacts" ? "contact-info__horizontal-line--contacts" : null)
-  const customColor = (props.position === "contacts" ? "contact-info__link--contacts" : null)
+function ContactInfo(props) {
+  const custonContactInfoClass = props.position === "contacts" ? "contacts" :
+    props.position === "footer" ? "footer" :
+    null;
 
-  return(
-    <div className='contact-info'>
-
-        <p className='app-text contact-info__section-title contact-info__photographer-section--title'>
-          {AppText.photographer}
-        </p>
-
-        <div className='contact-info__links-section'>
-          <a
-            className={`app-text app-link contact-info__link contact-info__telegram-link ${customColor}`}
-            href={AppText.photographerTelegramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.photographerTelegramLink.toLowerCase()}
-          </a>
-          <a
-            className={`app-text app-link contact-info__link contact-info__instagram-link ${customColor}`}
-            href={AppText.photographerImstagramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.photographerImstagramLink}
-          </a>
-        </div>
+  // const customInfoLink = props.position === "contacts" ?
+  //   "contact-info__link--contacts" :
+  //   props.position === "footer" ?
+  //   "contact-info__link--footer" :
+  //   null;
 
 
-        <hr className={`contact-info__horizontal-line ${custonMargin}`} />
 
+  return (
+    <div className={`contact-info contact-info--${custonContactInfoClass}`}>
 
-        <p className='app-text contact-info__section-title contact-info__agent-section--title'>
-          {AppText.agent}
-        </p>
+      <p className={`app-text contact-info__section-title contact-info__section-title--${custonContactInfoClass} contact-info__photographer-section--title`}>
+        {AppText.photographer}
+      </p>
 
-        <div className='contact-info__links-section'>
-          <a
-            className={`app-text app-link contact-info__link contact-info__telegram-link ${customColor}`}
-            href={AppText.agentTelegramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.agentTelegramLink}
-          </a>
-          <a
-            className={`app-text app-link contact-info__link contact-info__instagram-link ${customColor}`}
-            href={AppText.agentImstagramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.agentImstagramLink}
-          </a>
-        </div>
-
+      <div className={`contact-info__links-section contact-info__links-section--${custonContactInfoClass}`}>
+        <a
+          className={`app-text app-link contact-info__link contact-info__link--${custonContactInfoClass} contact-info__telegram-link`}
+          href={AppText.photographerTelegramLinkWay}
+          target="_blank"
+          rel="noopener noreferrer">
+          {AppText.photographerTelegramLink.toLowerCase()}
+        </a>
+        <a
+          className={`app-text app-link contact-info__link contact-info__link--${custonContactInfoClass} contact-info__instagram-link`}
+          href={AppText.photographerImstagramLinkWay}
+          target="_blank"
+          rel="noopener noreferrer">
+          {AppText.photographerImstagramLink}
+        </a>
       </div>
+
+
+      <hr className={`contact-info__horizontal-line contact-info__horizontal-line--${custonContactInfoClass}`} />
+
+
+      <p className={`app-text contact-info__section-title contact-info__section-title--${custonContactInfoClass} contact-info__agent-section--title`}>
+        {AppText.agent}
+      </p>
+
+      <div className={`contact-info__links-section contact-info__links-section--${custonContactInfoClass}`}>
+        <a
+          className={`app-text app-link contact-info__link contact-info__link--${custonContactInfoClass} contact-info__telegram-link`}
+          href={AppText.agentTelegramLinkWay}
+          target="_blank"
+          rel="noopener noreferrer">
+          {AppText.agentTelegramLink}
+        </a>
+        <a
+          className={`app-text app-link contact-info__link contact-info__link--${custonContactInfoClass} contact-info__instagram-link`}
+          href={AppText.agentImstagramLinkWay}
+          target="_blank"
+          rel="noopener noreferrer">
+          {AppText.agentImstagramLink}
+        </a>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,56 +11,7 @@ function Footer(props) {
       <h3 className='app-text footer__title'>
         {AppText.footerTitle}
       </h3>
-      <ContactInfo />
-      {/* <div className='footer__sections'>
-
-        <p className='app-text footer__section-title footer__photographer-section--title'>
-          {AppText.photographer}
-        </p>
-
-        <div className='footer__links-section'>
-          <a
-            className='app-text app-link footer__link footer__telegram-link'
-            href={AppText.photographerTelegramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.photographerTelegramLink.toLowerCase()}
-          </a>
-          <a
-            className='app-text app-link footer__link footer__instagram-link'
-            href={AppText.photographerImstagramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.photographerImstagramLink}
-          </a>
-        </div>
-
-
-        <hr className='footer__horizontal-line' />
-
-
-        <p className='app-text footer__section-title footer__agent-section--title'>
-          {AppText.agent}
-        </p>
-
-        <div className='footer__links-section'>
-          <a
-            className='app-text app-link footer__link footer__telegram-link'
-            href={AppText.agentTelegramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.agentTelegramLink}
-          </a>
-          <a
-            className='app-text app-link footer__link footer__instagram-link'
-            href={AppText.agentImstagramLinkWay}
-            target="_blank"
-            rel="noopener noreferrer">
-            {AppText.agentImstagramLink}
-          </a>
-        </div>
-
-      </div> */}
+      <ContactInfo position="footer" />
     </footer>
   );
 };
